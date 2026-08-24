a6aded3438fc36aae4cf856d0973b49f8f8993f2
Admin <chirukinbb@gmail.com>
2026-08-24 13:32:21 +0300

    refactor(events): simplify route middleware assignments and add unsubscribe endpoint
    - Removed ability middleware checks from event routes
    - Reorganized event owner middleware grouping for update and delete routes
    - Moved member routes inside event owner middleware group for clarity
    - Added post route for unsubscribe with reservable middleware
    - Removed redundant auth middleware from subscribe and unsubscribe routes
    - Simplified middleware usage on store and show routes for events

--- Modules/Events/routes/api.php
+++ Modules/Events/routes/api.php
@@ -1,28 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Events\Enums\PermissionEnum;
 use Modules\Events\Http\Controllers\Api\EventsController;
 use Modules\Events\Http\Controllers\Api\MemberController;
 use Modules\Events\Http\Middlewares\MemberMiddleware;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::get('events', [EventsController::class, 'index'])->middleware('ability:' . PermissionEnum::API_VIEW_EVENT_LIST->value)->middleware(\Modules\Events\Middlewares\HasFilterMiddleware::class);
+    Route::get('events', [EventsController::class, 'index'])->middleware(\Modules\Events\Middlewares\HasFilterMiddleware::class);
 
     Route::prefix('event/{event}')->middleware(\Modules\Events\Http\Middlewares\EventOwnerMiddleware::class)->group(function () {
-        Route::put('/', [EventsController::class, 'update'])->middleware('ability:' . PermissionEnum::API_EDIT_EVENT->value);
-        Route::delete('/', [EventsController::class, 'destroy'])->middleware('ability:' . PermissionEnum::API_CREATE_EVENT->value);
+        Route::middleware(\Modules\Events\Http\Middlewares\EventOwnerMiddleware::class)->group(function () {
+            Route::put('/', [EventsController::class, 'update']);
+            Route::delete('/', [EventsController::class, 'destroy']);
 
-        Route::post('subscribe', [MemberController::class, 'create'])->middleware(['auth:sanctum', \Modules\Events\Http\Middlewares\ReservableMiddleware::class]);
-
-        Route::prefix('member/{member}')->middleware(['auth:sanctum', MemberMiddleware::class])->group(function () {
-            Route::patch('', [MemberController::class, 'update']);
-            Route::delete('unsubscribe', [MemberController::class, 'destroy']);
+            Route::prefix('member/{member}')->middleware(['auth:sanctum', MemberMiddleware::class])->group(function () {
+                Route::patch('', [MemberController::class, 'update']);
+                Route::delete('unsubscribe', [MemberController::class, 'destroy']);
+            });
         });
+
+        Route::post('subscribe', [MemberController::class, 'create'])->middleware([\Modules\Events\Http\Middlewares\ReservableMiddleware::class]);
+        Route::post('unsubscribe', [MemberController::class, 'destroy'])->middleware([\Modules\Events\Http\Middlewares\ReservableMiddleware::class]);
     });
 
-    Route::post('events', [EventsController::class, 'store'])->middleware('ability:' . PermissionEnum::API_CREATE_EVENT->value);
-    Route::get('event/{event}', [EventsController::class, 'show'])->middleware('ability:' . PermissionEnum::API_VIEW_EVENT->value);
+    Route::post('events', [EventsController::class, 'store']);
+    Route::get('event/{event}', [EventsController::class, 'show']);
 });
 
 Route::get('v1/categories', [\Modules\Events\Http\Controllers\Api\CategoryController::class, 'index']);
